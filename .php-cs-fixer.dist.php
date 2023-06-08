dbff01a2db9ea391a9af640a881b3e04a9e604f1
Jérôme Gamez <jerome@gamez.name>
2023-06-08 12:38:58 +0200

    Use `beste/php-cs-fixer-config`

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,33 +2,18 @@
 
 declare(strict_types=1);
 
-$finder = PhpCsFixer\Finder::create()
-    ->in(['src', 'tests']);
+use Beste\PhpCsFixer\Config\Factory;
+use Beste\PhpCsFixer\Config\RuleSet\Php81;
 
-return (new PhpCsFixer\Config())
-    ->setRules([
-        '@Symfony' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => true,
-            'import_functions' => true,
-        ],
-        'header_comment' => ['header' => ''],
-        'ordered_imports' => true,
-        'phpdoc_align' => false,
-        'phpdoc_order' => true,
-        'php_unit_internal_class' => [
-            'types' => [
-                'abstract',
-                'final',
-                'normal',
-            ],
-        ],
-        'yoda_style' => [
-            'equal' => false,
-            'identical' => false,
-            'less_and_greater' => false,
-        ],
+$config = Factory::fromRuleSet(new Php81());
+
+$config
+    ->getFinder()
+    ->in([
+        'src',
+        'tests',
     ])
-    ->setFinder($finder);
+    ->ignoreDotFiles(false)
+;
+
+return $config;
